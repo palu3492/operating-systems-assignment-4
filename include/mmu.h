1fdee4f7ba8866da65a1af96121575819f313100
Alex Palumbo <palu3492@stthomas.edu>
2020-04-15 08:50:15 -0500

    Initial

---
+++ include/mmu.h
@@ -0,0 +1,33 @@
+#ifndef __MMU_H_
+#define __MMU_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef struct Variable {
+    std::string name;
+    int virtual_address;
+    int size;
+} Variable;
+
+typedef struct Process {
+    uint32_t pid;
+    std::vector<Variable*> variables;
+} Process;
+
+class Mmu {
+private:
+    uint32_t _next_pid;
+    int _max_size;
+    std::vector<Process*> _processes;
+
+public:
+    Mmu(int memory_size);
+    ~Mmu();
+
+    uint32_t createProcess();
+    void print();
+};
+
+#endif // __MMU_H_
---
+++ src/pagetable.cpp
@@ -0,0 +1,54 @@
+#include "pagetable.h"
+
+PageTable::PageTable(int page_size)
+{
+    _page_size = page_size;
+}
+
+PageTable::~PageTable()
+{
+}
+
+void PageTable::addEntry(uint32_t pid, int page_number)
+{
+    // Combination of pid and page number act as the key to look up frame number
+    std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
+
+    // Find free frame
+    // TODO: implement this!
+    int frame = 0; 
+    _table[entry] = frame;
+}
+
+int PageTable::getPhysicalAddress(uint32_t pid, int virtual_address)
+{
+    // Convert virtual address to page_number and page_offset
+    // TODO: implement this!
+    int page_number = 0;
+    int page_offset = 0;
+
+    // Combination of pid and page number act as the key to look up frame number
+    std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
+    
+    // If entry exists, look up frame number and convert virtual to physical address
+    int address = -1;
+    if (_table.count(entry) > 0)
+    {
+        // TODO: implement this!
+    }
+
+    return address;
+}
+
+void PageTable::print()
+{
+    std::map<std::string, int>::iterator it;
+
+    std::cout << " PID  | Page Number | Frame Number" << std::endl;
+    std::cout << "------+-------------+--------------" << std::endl;
+
+    for (it = _table.begin(); it != _table.end(); it++)
+    {
+        // TODO: print all pages
+    }
+}
